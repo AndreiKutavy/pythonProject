3fbe1d98844617095b8381b52549270ae4c14553
Andrei Kutavy <kutavyandrei@gmail.com>
2022-02-20 13:46:26 +0300

    Init commit

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
-# Здесь будет выведено: 3
-a = 1
-b = 2
-c = a + b
-print("Здесь будет выведено:", c)
+# Home work 2.1 The print() function
+print("Programming", "Essentials", "in...Python", sep="***")
 
-# Здесь будет выведено 'С': 3
-a = 1
-b = 2
-c = a + b
-print("Здесь будет выведено 'С': {}".format(c))
+# Home work 2.2 The print() function sep, end
+print("     ", end="*\n")
+print("    ", " ", sep="*", end="*\n")
+print("   ", "   ", sep="*", end="*\n")
+print("  ", "     ", sep="*", end="*\n")
+print("***", "***", sep="     ", end="\n")
+print("   ", "   ", sep="*", end="*\n")
+print("   ", "   ", sep="*", end="*\n")
+print("   ", end="*****\n")
 
-# Здесь будет выведено: 1 + 2 = 3
-a = 1
-b = 2
-c = a + b
-print("Здесь будет выведено: {a} + {b} = {c}".format(c=c, a=a, b=b))
+# Home work 2.3
+print(""""I'm"\n""Learning""\n""Python""")
 
-# Здесь будет выведено: 1 + 2 = 3
-a = 1
-b = 2
-c = a + b
-print(f"Здесь будет выведено: {a} + {b} = {c}")
+
+
+# Test
+# # Здесь будет выведено: 3
+# a = 1
+# b = 2
+# c = a + b
+# print("Здесь будет выведено:", c)
+#
+# # Здесь будет выведено 'С': 3
+# a = 1
+# b = 2
+# c = a + b
+# print("Здесь будет выведено 'С': {}".format(c))
+#
+# # Здесь будет выведено: 1 + 2 = 3
+# a = 1
+# b = 2
+# c = a + b
+# print("Здесь будет выведено: {a} + {b} = {c}".format(c=c, a=a, b=b))
+#
+# # Здесь будет выведено: 1 + 2 = 3
+# a = 1
+# b = 2
+# c = a + b
+# print(f"Здесь будет выведено: {a} + {b} = {c}")
